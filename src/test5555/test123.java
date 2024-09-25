8b4ac9b9a452e3f2a2257f7c453509007e40ae68
GEOSOFT <GEOSOFT@DESKTOP-87V8NFS>
2024-09-25 17:34:09 +0900

    test

---
+++ src/test5555/test123.java
@@ -0,0 +1,11 @@
+package test5555;
+
+public class test123 {
+
+	public static void main(String[] args) {
+		System.out.println("zzz");
+		System.out.println("alpha");
+
+	}
+
+}
---
+++ src/test5555/test4321ssss.java
@@ -0,0 +1,7 @@
+package test5555;
+
+public class test4321ssss {
+	public static void main(String args) {
+		System.out.println("Hello WOrld");
+	}
+}
